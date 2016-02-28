0fc060a6bb6828df3e0a770ea220cc9ca22c5224
sahalsajjad <sahalsajjad@gmail.com>
2016-02-28 17:33:51 +0530

    Faculty Bio Templates completed

--- iiits/models.py
+++ iiits/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 class Department(Model):
 	name = CharField(max_length=100)
 	code = CharField(db_index=True,max_length=20)
+	def __str__(self):
+		return self.name
 class FacultyTitle(Model):
 	title = CharField(max_length=100)
 	code = CharField(db_index=True,max_length=20)
@@ -13,7 +15,8 @@ class FacultyTitle(Model):
 class ResearchArea(Model):
 	title = CharField(max_length=150)
 	code = CharField(db_index=True,max_length=50)
-	
+	def __str__(self):
+		return self.title
 class Faculty(Model):
 	user = OneToOneField(User)
 	photo=ImageField(upload_to='iiits/static/iiits/images/faculty/')
@@ -23,12 +26,24 @@ class Faculty(Model):
 	contact_no=TextField()
 	professional_edu=TextField()
 	website=TextField()
+	other_info=TextField(default='NA')
+	achievements=TextField(default='NA')
 	public_uri_name=CharField(max_length=100, db_index=True,default='NA')
+
 
 	def getFullName(self):
 		return self.user.get_full_name()
 	def __str__(self):
 		return self.getFullName()	
+	def getAllResearchInterests(self):
+		results = list()
+		for x in self.research_areas:
+			try:
+				r = ResearchArea.objects.get(id=int(x))
+				results.appends(r)
+			except ObjectDoesNotExist:
+				donothing=True	
+		return results		
 
 class VisitingFaculty(Model):
 	user = OneToOneField(User)
