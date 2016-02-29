25848872fd91f3c81d51b3319a4557964ea6d018
sahalsajjad <sahalsajjad@gmail.com>
2016-02-29 15:17:47 +0530

    getAllFacultyByRA added

--- iiits/methods.py
+++ iiits/methods.py
@@ -1,4 +1,5 @@
 import re
+from iiits.models import * 
 def ifNone(obj, beautifier):
 	if obj is None:
 		return beautifier
@@ -9,3 +10,11 @@ def getPublicURI(path):
 	results = re.search(r'(?<=~)[A-Za-z_.]*',path)
 	print path
 	return results.group(0)
+
+def getAllFacultyByRA(raid):
+	F = Faculty.objects.all()
+	results = []
+	for f in F:
+		if	raid in f.research_areas:
+			results.append(f)
+	return results		
