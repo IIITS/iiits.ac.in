39d5479199e86331f0e85469775848577e51d5d7
sahal sajjad <sahalsajjad@gmail.com>
2016-08-11 22:38:19 +0530

    Global commit

--- iiits_config/wsgi.py
+++ iiits_config/wsgi.py
@@ -1,15 +1,13 @@
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 import os
-<<<<<<< HEAD
+
 
 os.environ['DJANGO_SETTINGS_MODULE'] ="iiits_config.settings"
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 os.environ['HTTPS'] = "on"
 
-=======
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 os.environ['HTTPS'] = "on"
->>>>>>> d91a6c4794ef75e9177cc93f14e31d22b9fe2cd0
